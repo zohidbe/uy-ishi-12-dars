50bd8d7734a1dfcdea0652f989960b8e86297aad
zohid <zohid.xayadarov77@gmail.com>
2024-01-27 07:38:12 +0500

    salom

---
+++ app.js
@@ -0,0 +1,32 @@
+// # JavaScript Misollar
+
+// ## Object va Metodlar
+
+// ```javascript
+// // Object va Metodlar
+// let person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 30,
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+
+// console.log("To'la ism:", person.fullName());
+
+
+
+
+// // Function va String Object
+// function greet(name) {
+//   return "Salom, " + name + "!";
+// }
+
+// console.log(greet("Ali"));
+
+// let text = "JavaScriptning quvvati";
+// console.log(text.toUpperCase()); // Barcha harflarni katta harfga o'tkazadi
+// console.log(text.length); // Matn uzunligi
+// console.log(text.indexOf("quvvati")); // "quvvati" so'zining boshlang'ich indeksi
+// console.log(text.slice(0, 10)); // 0 dan 10 gacha bo'lgan qismi olish
